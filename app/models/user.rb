b68a4b78e6cfd39e8a51ae2c5dbbc72ce0260aed
azizdevfull <aziz16110904@gmail.com>
2023-05-12 14:08:57 +0500

    Completed user's validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
 	has_secure_password
+	validates :name, presence: true
+	validates :email, uniqueness: true, presence: true
+	validates :password, length: { minimum: 6 }
 end
